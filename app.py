830297751e0ef292d1d1ba644b64b8d8faa37b93
aubarrio <aubarrios142@gmail.com>
2025-09-08 23:29:16 -0700

    components build

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import gradio as gr
 
-with gr.Blocks() as demo:
 
-    def do_something():
-        return "Test"
+with gr.Blocks() as demo:
+    # -------------------- Helper Functions --------------------
     
+    # ---------- Components ----------
+
+    #add build type component
+    build_type = gr.Radio(
+        ["Learning Path", "Tutorial"],
+        label="What do you wish to build today?"
+    )
+
 if __name__ == "__main__":
     demo.launch(show_error=True)
