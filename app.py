513bf19cd7a1e31e5f9029a15ccd3e2daeefb6f3
aubarrio <aubarrios142@gmail.com>
2025-09-08 23:29:46 -0700

    components build

--- app.py
+++ app.py
@@ -12,5 +12,20 @@ with gr.Blocks() as demo:
         label="What do you wish to build today?"
     )
 
+    #add learning path topic textbox
+    topic = gr.Textbox(visible=False)
+
+    #add difficulty selection component
+    difficulty = gr.Radio(visible=False)
+
+    #build selection gradio
+    radio = gr.Radio(visible=False)
+
+    #build chatbot interface
+    chatbot = gr.Chatbot(type="messages")
+
+    #build message textbox for chatbot
+    msg = gr.Textbox(visible=False)
+
 if __name__ == "__main__":
     demo.launch(show_error=True)
